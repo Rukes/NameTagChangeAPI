5b1bda40d0e80577fa986f6dc83ad00bfc1868c0
Rukes <samuel.kunert@seznam.cz>
2017-01-02 13:32:59 +0000

    Test.java

---
+++ Test.java
@@ -0,0 +1,19 @@
+package eu.rukes.testplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Test extends JavaPlugin implements Listener{
+    
+    @Override
+    public void onEnable(){}
+    @Override
+    public void onDisable(){}
+    
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        NameTagChangeAPI.changeName("§5§nAwesomeKing", e.getPlayer(), true);
+    }
+}
